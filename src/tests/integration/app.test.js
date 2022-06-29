82cf667f78d936a5472c478fce8813223129f951
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-06-28 22:40:03 -0300

    feat: refactor tests names & add last test

--- src/tests/integration/app.test.js
+++ src/tests/integration/app.test.js
@@ -14,7 +14,7 @@ afterAll(async () => {
 
 describe('GET /boleto/:billetCode', () => {
   // cenários felizes
-  test('GET /boleto/21290001192110001210904475617405975870000002000', async () => {
+  test('GET billet bank valid', async () => {
     const response = await request
       .get('/boleto/21290001192110001210904475617405975870000002000');
 
@@ -24,7 +24,7 @@ describe('GET /boleto/:billetCode', () => {
     expect(response.body.expirationDate).toEqual('2018-07-16');
   });
 
-  test('GET /boleto/34190010070058744123941234510000781210000000002', async () => {
+  test('GET billet bank amount less than 1.00', async () => {
     const response = await request
       .get('/boleto/34190010070058744123941234510000781210000000002');
 
@@ -34,7 +34,7 @@ describe('GET /boleto/:billetCode', () => {
     expect(response.body.expirationDate).toEqual('2020-01-01');
   });
 
-  test('GET /boleto/856200000037194300042026204304030057713141812105', async () => {
+  test('GET billet tax valid with mod10 calculation', async () => {
     const response = await request
       .get('/boleto/856200000037194300042026204304030057713141812105');
 
@@ -44,7 +44,7 @@ describe('GET /boleto/:billetCode', () => {
     expect(response.body.expirationDate).toEqual('2022-04-30');
   });
 
-  test('GET /boleto/896100000000599800010119053332010064260000157446', async () => {
+  test('GET billet tax valid with mod11 calculation', async () => {
     const response = await request
       .get('/boleto/896100000000599800010119053332010064260000157446');
 
@@ -55,7 +55,7 @@ describe('GET /boleto/:billetCode', () => {
   });
 
   // cenários não felizes
-  test('GET /boleto/2129000119211000121090447561740597587000000200', async () => {
+  test('GET billet with less than 47 digits', async () => {
     const response = await request
       .get('/boleto/2129000119211000121090447561740597587000000200');
 
@@ -63,15 +63,15 @@ describe('GET /boleto/:billetCode', () => {
     expect(response.body.message).toEqual('Boleto inválido: linha digitada não possui a quantidade correta de caracteres.');
   });
 
-  test('GET /boleto/212900011921100012109044756174059758700000020fg', async () => {
+  test('GET billet with digits in it that are not number', async () => {
     const response = await request
-      .get('/boleto/212900011921100012109044756174059758700000020fg');
+      .get('/boleto/2129_0011@2110rt12109-44756174059758700000020fg');
 
     expect(response.statusCode).toEqual(400);
     expect(response.body.message).toEqual('Boleto inválido: linha digitada do boleto deve conter apenas números.');
   });
 
-  test('GET /boleto/00190500924014481606906809350314337370000000100', async () => {
+  test('GET billet bank with invalid digits', async () => {
     const response = await request
       .get('/boleto/00190500924014481606906809350314337370000000100');
 
@@ -79,7 +79,7 @@ describe('GET /boleto/:billetCode', () => {
     expect(response.body.message).toEqual('Boleto inválido: dígitos verificadores inválidos.');
   });
 
-  test('GET /boleto/856200000037194300042025204304030057713141812105', async () => {
+  test('GET billet tax with invalid digits', async () => {
     const response = await request
       .get('/boleto/856200000037194300042025204304030057713141812105');
 
@@ -87,7 +87,7 @@ describe('GET /boleto/:billetCode', () => {
     expect(response.body.message).toEqual('Boleto inválido: dígitos verificadores inválidos.');
   });
 
-  test('GET /boleto/21290001192110001210904475617435975870000002000', async () => {
+  test('GET billet bank with invalid general digit', async () => {
     const response = await request
       .get('/boleto/21290001192110001210904475617435975870000002000');
 
@@ -95,9 +95,17 @@ describe('GET /boleto/:billetCode', () => {
     expect(response.body.message).toEqual('Boleto inválido: dígito verificador geral inválido.');
   });
 
-  test('GET /boleto/856200000037194300442026204304030057713141812105', async () => {
+  test('GET billet tax with invalid general digit mod10', async () => {
     const response = await request
       .get('/boleto/856200000037194300442026204304030057713141812105');
+
+    expect(response.statusCode).toEqual(400);
+    expect(response.body.message).toEqual('Boleto inválido: dígito verificador geral inválido.');
+  });
+
+  test('GET billet tax with invalid general digit mod11', async () => {
+    const response = await request
+      .get('/boleto/896200000000599800010119053332010064260000157446');
 
     expect(response.statusCode).toEqual(400);
     expect(response.body.message).toEqual('Boleto inválido: dígito verificador geral inválido.');
